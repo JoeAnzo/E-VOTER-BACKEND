eb492ab0d9436bcd1b46122b5ff6176bbcba4f2c
JoeAnzo <joelanzovule@gmail.com>
2026-03-29 11:16:14 -0700

    Added env file

--- Config/database.js
+++ Config/database.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv"
+dotenv.config()
 import mongoose from "mongoose"
 async function connectDatabase(){
     try {
-        await mongoose.connect('mongodb://0.0.0.0:27017/School')
+        await mongoose.connect(process.env.DATABASE_URL)
         console.log('Database Connected Successfully')
     } catch (error) {
         console.log('Error:',error.message)
