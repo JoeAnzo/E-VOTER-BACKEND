0a89889327c9ec410ee23ffd818a13d8145a36c2
JoeAnzo <joelanzovule@gmail.com>
2026-03-29 11:31:26 -0700

    added port to .env

--- Server.js
+++ Server.js
@@ -4,8 +4,11 @@ import studentsRouter from './Routes/studentsRoutes.js'
 import candidatesRouter from './Routes/candidatesRoutes.js'
 import adminRouter from './Routes/adminRoutes.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
@@ -13,8 +16,7 @@ app.use('/v1/api/Students',studentsRouter)
 app.use('/v1/api/Candidates',candidatesRouter)
 app.use('/v1/api/admin',adminRouter)
 
-const PORT = 3000
-app.listen(PORT,'0.0.0.0',async ()=>{
+app.listen(PORT,async ()=>{
     await connectDatabase()
     console.log(`Server listening on port ${PORT}`);  
 })
